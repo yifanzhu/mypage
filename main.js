a826de3670ed99b23e2aec0fde005e1f34f0edb4
Yifan Zhu <yifan.zhu@nih.gov>
2017-01-27 16:20:03 -0500

    clean up styles

--- main.js
+++ main.js
@@ -5,14 +5,10 @@ require.config({
         'angular-animate': 'bower_components/angular-animate/angular-animate.min',
         'angular-aria': 'bower_components/angular-aria/angular-aria.min',
         'angular-bootstrap': 'bower_components/angular-bootstrap/ui-bootstrap-tpls',
-        'angular-cookies': 'bower_components/angular-cookies/angular-cookies.min',
-        'ckeditor-core':'bower_components/ckeditor/ckeditor',
-        'ng-ckeditor': 'bower_components/ng-ckeditor/dist/ng-ckeditor.min',
         'angular-material': 'bower_components/angular-material/angular-material.min',
         'angular-material-icons': 'bower_components/angular-material-icons/angular-material-icons.min',
         'angular-messages': 'bower_components/angular-messages/angular-messages.min',
         'angular-sanitize': 'bower_components/angular-sanitize/angular-sanitize.min',
-        'angular-ui-router': 'bower_components/angular-ui-router/release/angular-ui-router.min',
         jquery: 'bower_components/jquery/dist/jquery.min',
         lodash: 'bower_components/lodash/lodash'
     },
@@ -34,27 +30,10 @@ require.config({
         'angular-bootstrap': [
             'angular'
         ],
-        'angular-cookies': [
-            'angular'
-        ],
-        'ckeditor-core': {
-            deps: [
-                'jquery'
-            ],
-            exports: 'CKEDITOR'
-        },
-        'ng-ckeditor': [
-            'angular',
-            'jquery',
-            'ckeditor-core'
-        ],
         'angular-sanitize': [
             'angular'
         ],
         'angular-messages': [
-            'angular'
-        ],
-        'angular-ui-router': [
             'angular'
         ],
         'angular-material': {
