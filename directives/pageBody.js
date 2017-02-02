953fee07d525430c91c39055f4dcbcfba5189681
yifan <zyfacatch@gmail.com>
2017-02-01 20:21:15 -0500

    update info

--- directives/pageBody.js
+++ directives/pageBody.js
@@ -29,21 +29,21 @@ define(['./module', 'google-maps'], function (app, googleLoader) {
                 projects: [
                     {
                         id:0,
-                        title: 'Consumables Management System(Mobile Version)',
-                        image: 'https://res.cloudinary.com/dihnc49qr/image/upload/v1478314764/photo-1453928582365-b6ad33cbcf64_t0ixly.jpg',
-                        description: 'iOS and Android version of Consumables Management System.'
-                    },
-                    {
-                        id:1,
                         title: 'Consumables Management System',
                         image: 'http://res.cloudinary.com/dihnc49qr/image/upload/v1485975509/Screen_Shot_2017-02-01_at_1.33.19_PM_eood75.png',
                         description: 'A Check-In and Check-Out system which can track consumables in and out, generate barcodes for consumables, assign locations to consumables and check stock level for consumables.'
                     },
                     {
-                        id:2,
+                        id:1,
                         title: 'Freezer Management System',
                         image: 'http://res.cloudinary.com/dihnc49qr/image/upload/v1485975509/Screen_Shot_2017-02-01_at_1.34.53_PM_zyvrkb.png',
                         description: 'An application which can help user map and manage relations among freezers, donors and derivatives.'
+                    },
+                    {
+                        id:2,
+                        title: 'Consumables Management System(Mobile Version)',
+                        image: 'http://res.cloudinary.com/dihnc49qr/image/upload/v1485975509/Screen_Shot_2017-02-01_at_1.33.19_PM_eood75.png',
+                        description: 'iOS and Android version of Consumables Management System.'
                     }
                 ]
             };
@@ -53,16 +53,22 @@ define(['./module', 'google-maps'], function (app, googleLoader) {
                     {
                         id:0,
                         name: 'My Page',
-                        image: 'https://res.cloudinary.com/dihnc49qr/image/upload/v1478314764/photo-1453928582365-b6ad33cbcf64_t0ixly.jpg',
+                        image: 'http://res.cloudinary.com/dihnc49qr/image/upload/v1478314597/photo-1432821596592-e2c18b78144f_l3smw2.jpg',
                         sourceCode: 'https://github.com/yifanzhu/mypage',
                         page: 'https://yifanpage.herokuapp.com'
                     },
                     {
                         id:1,
-                        name: 'My Blog',
-                        image: 'https://res.cloudinary.com/dihnc49qr/image/upload/v1478314764/photo-1453928582365-b6ad33cbcf64_t0ixly.jpg',
-                        sourceCode: 'https://github.com/yifanzhu/myblog',
+                        name: 'My Moment',
+                        image: 'http://res.cloudinary.com/dihnc49qr/image/upload/v1485998231/Screen_Shot_2017-02-01_at_8.16.52_PM_csgvff.png',
+                        sourceCode: 'https://github.com/yifanzhu/mymoment',
                         page: 'https://yifanblog.herokuapp.com'
+                    },
+                    {
+                        id:1,
+                        name: 'My Blog',
+                        image: 'http://res.cloudinary.com/dihnc49qr/image/upload/v1478314505/photo-1429051781835-9f2c0a9df6e4_u9fduy.jpg',
+                        sourceCode: 'https://github.com/yifanzhu/myblog'
                     }
                 ]
             };
